f666e335cbb0ed6852709ad2e547e18218fceb27
yure <yurenia1987@gmail.com>
2019-06-06 13:48:22 -0400

    Set properties by default and the source and target languages

--- src/main/java/com/example/DriverController.java
+++ src/main/java/com/example/DriverController.java
@@ -4,36 +4,71 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Paths;
+
 public class DriverController {
 
-    private String driverPath = "C:\\Users\\Name\\Documents\\yure_2019\\testing\\gecko_driver\\";
     private WebDriver driver;
 
     //selectors
-    private static String source = "#source";
-    private static String translation = ".tlid-translation.translation";
+    private By source = By.cssSelector("#source");
+    private By translation = By.cssSelector(".tlid-translation.translation");
+    private By div_languageFrom = By.cssSelector(".sl-sugg > div:nth-child(2) > div:nth-child(1)");
+    private By slSearchBox = By.cssSelector("#sl_list-search-box");
+    private By esCheckmark = By.cssSelector(".language_list_item_icon.sl_list_es_s_checkmark");
 
-    public void launchBrowser(String wordToTranslate){
+    private By div_languageTo = By.cssSelector(".tl-sugg > div:nth-child(2) > div:nth-child(1)");
+    private By tlSearchBox = By.cssSelector("#tl_list-search-box");
+    private By enCheckmark = By.cssSelector(".language_list_item_icon.tl_list_en_s_checkmark");
+
+    public void launchBrowser(String languageFrom, String languageTo, String wordToTranslate){
         String url = "https://translate.google.com/";
-        System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver.exe");
-        driver = new FirefoxDriver();
 
+        driver = new FirefoxDriver();
         driver.navigate().to(url);
-        driver.findElement(By.cssSelector(source)).sendKeys(wordToTranslate);
+
+        //language from
+        driver.findElement(div_languageFrom).click();
+        driver.findElement(slSearchBox).click();
+        driver.findElement(slSearchBox).sendKeys(languageFrom);
+        driver.findElement(esCheckmark).click();
+
+        //languaje to
+        driver.findElement(div_languageTo).click();
+        driver.findElement(tlSearchBox).click();
+        driver.findElement(tlSearchBox).sendKeys(languageTo);
+        driver.findElement(enCheckmark).click();
+
+        driver.findElement(source).sendKeys(wordToTranslate);
 
         sleep(3000);
 
-        System.out.println("Translated to English: " + driver.findElement(By.cssSelector(translation)).getText());
+        System.out.println("\n\n Translated to English: " + driver.findElement(translation).getText() + "\n\n");
+
         closeDriver();
     }
 
     public void closeDriver() {
-        if(driver!=null) {
+        if(driver != null) {
             driver.close();
+
+            if(driver != null) {  //I kill the driver process
+                String geckoName = Paths.get(System.getProperty("webdriver.gecko.driver")).getFileName().toString();
+
+                try {
+                    Runtime.getRuntime().exec("taskkill /F /IM " + geckoName + " /T");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                sleep(5000); // wait two seconds, for later, to delete the temporary driver if was created
+            }
         }
     }
 
-    private void sleep(long seconds){
+    public void sleep(long seconds){
         try {
             Thread.sleep(seconds);
         } catch (InterruptedException e){
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,19 +1,69 @@
 package com.example;
 
-import org.openqa.selenium.WebDriver;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class Main {
-    private static String driverPath = "C:\\Users\\Name\\Documents\\yure_2019\\testing\\gecko_driver\\";
-    private static WebDriver driver;
+    private static File tempFile;
 
-    private static String source = "#source";
-    private static String translation = ".tlid-translation.translation";
+    public static void main(String[] args) throws IOException{
+        setProperties();
 
-    public static void main(String[] words) {
-        int cont = 1;
-        for (String word : words){
-            DriverController sc = new DriverController();
-            sc.launchBrowser(word);
+        String languageFrom = args[0];
+        String languageTo = args[1];
+        String wordToTranslate = args[2];
+
+        DriverController controller = new DriverController();
+        controller.launchBrowser(languageFrom, languageTo, wordToTranslate);
+
+        if (tempFile != null){
+            tempFile.delete();
         }
+    }
+
+    private static void setProperties() {
+        //logging
+        if (System.getProperty("java.util.logging.config.file") == null){
+            InputStream input = Main.class.getResourceAsStream("/logging.properties");
+            try {
+                LogManager.getLogManager().readConfiguration(input);
+            } catch (IOException io){
+                io.printStackTrace();
+            }
+        }
+
+        //browser logs
+        if (System.getProperty("webdriver.firefox.logfile") == null){
+            System.setProperty("webdriver.firefox.logfile", "/dev/null");
+        }
+
+        //geckodriver path
+        if (System.getProperty("webdriver.gecko.driver") == null){
+            tempFile = getTempGeckoFile();
+            System.setProperty("webdriver.gecko.driver", tempFile.getPath());
+        }
+    }
+
+    private static File getTempGeckoFile(){
+        InputStream inputStream = Main.class.getResourceAsStream("/geckodriver.exe");
+
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("checkodriver", ".exe");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return null;
+        }
+
+        try {
+            Files.copy(inputStream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return null;
+        }
+        return tempFile;
     }
 }
